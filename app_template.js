aa89e6df55542136fb0f27d31e4c0715bf1295a5
WizAom <Aom_ll2555@hotmail.com>
2021-01-12 14:00:00 +0700

    DIE

--- app_template.js
+++ app_template.js
@@ -1,1 +1,18 @@
-//your code here!
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('form').onsubmit = function(){
+        const newRow = document.createElement('tr')
+        let row = document.createElement("span")
+        row = document.querySelector("#input-row").value
+        let col = document.createElement("span")
+        col = document.querySelector("#input-col").value
+        let id = document.createElement('span')
+        id = document.querySelector("#input-number").value
+        delBtn.innerHTML = 'button-compute'
+        delBtn.onclick = function(){
+            newRow.append("61063110"+id)
+        }
+        if(row%2==1){
+            
+        }
+    }
+}
